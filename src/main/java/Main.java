3be3b1c66279f670753af0cf0fe8b218c597ca2a
Mayank Lohani <mayankprakash.lohani@unkbi.com>
2025-05-30 21:40:22 +0530

    type command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        while (true) {
+        outerLoop: while (true) {
             System.out.print("$ ");
 
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             
-            if( input.equals("exit 0") ){
-                break;
+            List<String> command_list = new ArrayList<>(Arrays.asList("echo","exit"));
+            
+            switch(input){
+                case "exit 0":
+                    break outerLoop;
+                default:
+                    if (input.contains("type")){
+                        String command = input.substring(5,input.length());
+                        System.out.println(command + " is a shell builtin");
+                    }
+                    else if (input.contains("echo")) {
+                        System.out.println(input.substring(5,input.length()));
+                    }
+                    else{
+                        System.out.println(input + ": command not found");
+                    }
             }
-            else if(input.contains("echo")){
-                System.out.println(input.substring(5,input.length()));
-            }
-            else{
-                System.out.println(input + ": command not found");
-            }
+            
+            // if( input.equals("exit 0") ){
+            //     break;
+            // }
+            // else if(input.contains("echo")){
+            //     System.out.println(input.substring(5,input.length()));
+            // }
+            // else{
+            //     System.out.println(input + ": command not found");
+            // }
         }
     }
 }
