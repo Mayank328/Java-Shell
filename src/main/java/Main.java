1bef0e943b245b2b6c56c45387cb7e3f62ae5d78
Mayank Lohani <mayankprakash.lohani@unkbi.com>
2025-06-06 02:55:41 +0530

    quotes handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+    public static List<String> parseInput(String input){
+        List<String> tokens = new ArrayList<>();
+        Matcher m = Pattern.compile("'([^']*)'|\"([^\"]*)\"|(\\S+)").matcher(input);
+        
+        while(m.find()){
+            if(m.group(1)!=null){
+                tokens.add(m.group(1));
+            }
+            else if(m.group(2)!=null){
+                tokens.add(m.group(2));
+            }else{
+                tokens.add(m.group(3));
+            }
+        }
+        return tokens;
+    }
     public static void main(String[] args) throws Exception {
+        
         File currentDir = new File(System.getProperty("user.dir"));
         List<String> command_list = new ArrayList<>(Arrays.asList("echo","exit","type","pwd"));
+
         String pathEnv = System.getenv("PATH");
         String pathSeparator = System.getProperty("path.separator");
         String fileSeparator = System.getProperty("file.separator");
@@ -15,13 +35,16 @@ public class Main {
 
         outerLoop: while (true) {
             System.out.print("$ ");
-
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            List<String> command_and_args = parseInput(input);
 
-            String[] command_and_args = input.trim().split("\\s+");
-            String command = command_and_args[0];
-            String[] command_args = Arrays.copyOfRange(command_and_args,1,command_and_args.length);
+            // String[] command_and_args = input.trim().split("\\s+");
+            // String command = command_and_args[0];
+            // String[] command_args = Arrays.copyOfRange(command_and_args,1,command_and_args.length);
+            String command = command_and_args.get(0);
+            List<String> command_args_list = command_and_args.subList(1,command_and_args.size());
+            String[] command_args = command_args_list.toArray(new String[0]);
             
             switch(command){
                 case "exit 0":
@@ -31,23 +54,23 @@ public class Main {
                     break;
                 default:
                     if (command.equals("type")){
-                        String commandName = String.join(" ",command_args);
-                        if(command_list.contains(commandName)){
-                            System.out.println(commandName + " is a shell builtin");
+                        String argName = String.join(" ",command_args);
+                        if(command_list.contains(argName)){
+                            System.out.println(argName + " is a shell builtin");
                         }
                         else{
                             boolean notFound = true;
 
                             for (String dir : paths){
-                                File file = new File(dir + fileSeparator + commandName);
+                                File file = new File(dir + fileSeparator + argName);
                                 if(file.exists() && file.canExecute()){
-                                    System.out.println(commandName + " is " + file.getAbsoluteFile());
+                                    System.out.println(argName + " is " + file.getAbsoluteFile());
                                     notFound = false;
                                     break;
                                 }
                             }
                             if (notFound){
-                                System.out.println(commandName + ": not found");
+                                System.out.println(argName + ": not found");
                             }
                         }
                         
@@ -70,7 +93,6 @@ public class Main {
                         else{
                             System.out.println(command+": "+ command_args[0] + ": No such file or directory");
                         }
-
                     }
                     else{
                         String executable = null;
