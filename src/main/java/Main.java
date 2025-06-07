328daa503341e23923c4f0c20a818a1112b29f26
Mayank Lohani <mayankprakash.lohani@unkbi.com>
2025-06-07 15:36:14 +0530

    backslash quoting added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,8 +17,33 @@ public class Main {
 
             if(c == '\\'){
                 if(i+1 < input.length()){
-                    i+=1;
-                    current.append(input.charAt(i));
+                    char next = input.charAt(++i);
+
+                    switch (next) {
+                        case 'n': current.append('\n'); break;
+                        case 't': current.append('\t'); break;
+                        case 'r': current.append('\r'); break;
+                        case 'b': current.append('\b'); break;
+                        case 'f': current.append('\f'); break;
+                        case '\\': current.append('\\'); break;
+                        case '\'': current.append('\''); break;
+                        case '\"': current.append('\"'); break;
+
+                        case '0', '1', '2', '3','4', '5', '6', '7':
+                            StringBuilder oct = new StringBuilder();
+                            oct.append(next);
+                            for(int j = 0; j<2 && i+1 <input.length();j+=1){
+                                char oc = input.charAt(i+1);
+                                if( oc>= '0' && oc <='7'){
+                                    oct.append(oc);
+                                    i+=1;
+                                }else break;
+                            }
+                            current.append((char) Integer.parseInt(oct.toString(),8));
+                            break;
+                        default:
+                            current.append(next); 
+                    }
                 }
                 continue;
             }
@@ -59,7 +84,9 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+
             if(input.equals("exit 0")) break;
+            
             List<String> command_and_args = parseInput(input);
 
             String command = command_and_args.get(0);
